84f685e7e3f21a9d302e5332c9e2f618acfacbd6
Squirrel <5260850@gmail.com>
2023-06-23 19:05:12 +0300

    Add Home Work 4 Task 2

---
+++ HW_4_Task_2.java
@@ -0,0 +1,30 @@
+/**
+ *
+ * @param string s containing just the characters '(', ')', '{', '}', '[' and ']'
+ * @return false or true.
+ * @apiNote determine the input string.
+ */
+
+
+public class HW_4_Task_2 {
+    public static void main(String[] args) {
+        String s = "()[]{}";
+        System.out.println(isValid(s));
+
+    }
+
+    public static boolean isValid(String s) {
+        char[] charArray = new char[s.length()];
+        charArray = s.toCharArray();
+        for (int i = 0; i < charArray.length; i += 2) {
+            if (charArray[i] == '(' && charArray[i + 1] != ')') {
+                return false;
+            } else if (charArray[i] == '[' && charArray[i + 1] != ']') {
+                return false;
+            } else if (charArray[i] == '{' && charArray[i + 1] != '}') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
